a03af7e5e66525bfe97418d709c192b9576e431c
Romain Dolbeau <romain@dolbeau.org>
2015-10-10 05:17:08 +0100

    glib 2.16.6

---
+++ glib/build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=glib
+baseversion=2.16
+version=${baseversion}.6
+pkgver=1
+source[0]=http://ftp.gnome.org/pub/gnome/sources/glib/${baseversion}/$topdir-$version.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+configure_args+=(--with-libiconv-prefix=$prefix --with-libiconv=gnu --with-libintl-prefix=$prefix)
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS NEWS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
