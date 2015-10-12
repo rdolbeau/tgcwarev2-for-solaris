628a33cebedd0a80cccd7eb67b5a44dd911b1eda
Romain Dolbeau <romain@dolbeau.org>
2015-10-12 14:46:23 +0100

    libpng: rpath, description

--- libpng/build.sh
+++ libpng/build.sh
@@ -15,6 +15,9 @@ source[0]=http://download.sourceforge.net/libpng/$topdir-$version.tar.gz
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+
 reg prep
 prep()
 {
