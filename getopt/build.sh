bb75891d783980bc233f34e1dd0f1eba62f2c9c9
Romain Dolbeau <romain@dolbeau.org>
2015-10-14 16:34:21 +0100

    getopt 2.5, relocatable

--- getopt/build.sh
+++ getopt/build.sh
@@ -7,14 +7,16 @@
 # Check the following 4 variables before running the script
 topdir=getopt
 version=2.5
-pkgver=1
+pkgver=2
 source[0]=http://ftp.math.utah.edu/pub/${topdir}/${topdir}-${version}.tar.gz
 # If there are no patches, simply comment this
+#DESTDIR
 patch[0]=getopt25-001
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
+export CFLAGS=-fPIC
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 
