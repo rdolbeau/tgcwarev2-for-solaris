ecd66e4640a0e22e02806a3b80920d7fa5a432ee
Romain Dolbeau <romain@dolbeau.org>
2015-10-14 18:06:56 +0100

    WindowMaker 0.95.2 (but, unstable)

--- WindowMaker/build.sh
+++ WindowMaker/build.sh
@@ -6,21 +6,31 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=WindowMaker
-version=0.92.0
+version=0.95.2
 pkgver=1
 source[0]=http://windowmaker.org/pub/source/release/$topdir-$version.tar.gz
 # If there are no patches, simply comment this
 #Recent PNG
-patch[0]=wm092-001
+#patch[0]=wm092-001
 #Recent GIF
-patch[1]=wm092-002
+#patch[1]=wm092-002
+
+#Recent GIF
+patch[0]=wm0952-001
+#no X11/Xarch.h
+patch[1]=wm0952-002
+#missing setenv#superseded by GNU getopt
+#patch[2]=wm0952-003
+patch[2]=""
+#no __progname in Solaris
+patch[3]=wm0952-004
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
-export CFLAGS="-D__STDC__=1 -fPIC"
+#export CFLAGS="-D__STDC__=1 -fPIC"
 export CPPFLAGS="-I$prefix/include"
-export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib -lrt -lgetopt"
 
 reg prep
 prep()
