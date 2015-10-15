62bb5dc0b50874c47c1211aa852e446c49fa5a7d
Romain Dolbeau <romain@dolbeau.org>
2015-10-15 18:46:18 +0100

    itstool 2.0.2

---
+++ itstool/build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=itstool
+version=2.0.2
+pkgver=1
+source[0]=http://files.itstool.org/itstool/${topdir}-${version}.tar.bz2
+# If there are no patches, simply comment this
+#$PYTHON not python
+patch[0]=itstool202-001
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+export PYTHON=$prefix/bin/python2.7
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS NEWS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
