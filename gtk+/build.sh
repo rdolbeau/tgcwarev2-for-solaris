690e4ad28f45d5a051bb1818e948e1d644418de1
Romain Dolbeau <romain@dolbeau.org>
2015-10-19 18:45:41 +0100

    GTK+ 2.16.6

---
+++ gtk+/build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=gtk+
+baseversion=2.16
+version=${baseversion}.6
+pkgver=1
+source[0]=http://ftp.acc.umu.se/pub/gnome/sources/gtk+/${baseversion}/${topdir}-${version}.tar.gz
+# If there are no patches, simply comment this
+#recent PNG
+patch[0]=gtk+2166-001
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+
+configure_args+=(--disable-xkb)
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS NEWS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
