107060aa351daaea549b27e8f6e9bebf49ba96d3
Romain Dolbeau <romain@dolbeau.org>
2015-10-22 16:51:53 +0100

    push pixman to 0.32.8

--- pixman/build.sh
+++ pixman/build.sh
@@ -6,11 +6,14 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=pixman
-version=0.18.0
+version=0.32.8
 pkgver=1
 source[0]=http://cairographics.org/releases/${topdir}-${version}.tar.gz
 # If there are no patches, simply comment this
-#patch[0]=
+#remove MMAP test (we have mmap, but not MAP_ANON)
+patch[0]=pixman0328-001
+#libm doesn't have powf, only pow
+patch[1]=pixman0328-002
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
