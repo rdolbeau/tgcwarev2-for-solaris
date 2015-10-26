bc1d2ec83b9df41b065b84ec31b500433a1faf73
Romain Dolbeau <romain@dolbeau.org>
2015-10-26 14:10:07 +0000

    push pango to 1.31.2

--- pango/build.sh
+++ pango/build.sh
@@ -6,18 +6,22 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=pango
-baseversion=1.20
-version=${baseversion}.5
+baseversion=1.31
+version=${baseversion}.2
 pkgver=1
-source[0]=http://ftp.gnome.org/pub/gnome/sources/${topdir}/${baseversion}/$topdir-$version.tar.bz2
+source[0]=http://ftp.gnome.org/pub/gnome/sources/${topdir}/${baseversion}/$topdir-$version.tar.xz
 # If there are no patches, simply comment this
-#patch[0]=
+#Adjust to HarfBuzz 0.9.7 API
+patch[0]=pango1312-001
+#missng Xfuncproto.h
+patch[1]=pango1312-002
 
 # Source function library
 . ${BUILDPKG_SCRIPTS}/buildpkg.functions
 
 configure_args+=(--with-libiconv=gnu)
 
+export CFLAGS="-D__STDC__=1 -fPIC"
 export CPPFLAGS="-I$prefix/include"
 export LDFLAGS="-L$prefix/lib -R$prefix/lib"
 export PYTHON=$prefix/bin/python2.7
