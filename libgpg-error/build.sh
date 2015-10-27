7ef10caa9defc8cba3a42eff772f991fd9625205
Romain Dolbeau <romain@dolbeau.org>
2015-10-27 16:23:52 +0000

    push libgpg-error to 1.20

--- libgpg-error/build.sh
+++ libgpg-error/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=libgpg-error
-version=1.9
+version=1.20
 pkgver=1
 source[0]=ftp://ftp.gnupg.org/gcrypt/libgpg-error/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
@@ -17,7 +17,7 @@ source[0]=ftp://ftp.gnupg.org/gcrypt/libgpg-error/$topdir-$version.tar.bz2
 
 # Global settings
 export CPPFLAGS="-I$prefix/include"
-export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib -lrt"
 configure_args+=(--disable-static --disable-languages)
 
 reg prep
