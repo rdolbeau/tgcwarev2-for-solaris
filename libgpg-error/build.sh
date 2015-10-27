4412068c19bb458cabfce1d9ad8a505440c38f9c
Romain Dolbeau <romain@dolbeau.org>
2015-10-27 15:48:27 +0000

    libgpg-error update to 1.9

--- libgpg-error/build.sh
+++ libgpg-error/build.sh
@@ -6,7 +6,7 @@
 ###########################################################
 # Check the following 4 variables before running the script
 topdir=libgpg-error
-version=1.6
+version=1.9
 pkgver=1
 source[0]=ftp://ftp.gnupg.org/gcrypt/libgpg-error/$topdir-$version.tar.bz2
 # If there are no patches, simply comment this
