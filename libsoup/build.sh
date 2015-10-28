16abe6098d0facaf9546e68130d28ef5b78bc6eb
Romain Dolbeau <romain@dolbeau.org>
2015-10-28 15:02:22 +0000

    libsoup 2.53.1

---
+++ libsoup/build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=libsoup
+baseversion=2.53
+version=${baseversion}.1
+pkgver=1
+source[0]=http://ftp.gnome.org/pub/gnome/sources/${topdir}/${baseversion}/$topdir-$version.tar.xz
+# If there are no patches, simply comment this
+patch[0]=libsoup2531-001
+patch[1]=libsoup2531-002
+patch[2]=libsoup2531-003
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+configure_args+=(--with-libiconv=gnu --enable-introspection=no --enable-vala=no)
+
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+export PYTHON=$prefix/bin/python2.7
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS NEWS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
