1f9116bb36e5d590e3886644959a2840d8f4234b
Romain Dolbeau <romain@dolbeau.org>
2015-10-29 09:05:14 +0000

    json-glib 1.0.4

---
+++ json-glib/build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=json-glib
+version=1.0.4
+pkgver=1
+source[0]=https://download.gnome.org/sources/${topdir}/1.0/${topdir}-${version}.tar.xz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+configure_args+=(--with-xml-catalog=${prefix}/etc/xml/catalog.xml --enable-introspection=no)
+
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+export PYTHON=$prefix/bin/python2.7
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS NEWS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
