cf7781218bd61d824788cc772325a09c93f6254b
Romain Dolbeau <romain@dolbeau.org>
2015-10-30 15:31:01 +0000

    dbus 1.8.20

---
+++ dbus/build.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=dbus
+version=1.8.20
+pkgver=1
+source[0]=http://${topdir}.freedesktop.org/releases/${topdir}/${topdir}-${version}.tar.gz
+# If there are no patches, simply comment this
+# missing ipv6
+patch[0]=dbus1820-001
+patch[1]=dbus1820-002
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+#configure_args+=
+
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib -lresolv"
+export PYTHON=$prefix/bin/python2.7
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS NEWS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
