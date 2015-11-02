5afdb42ff1ed1c0a46573bd4b6db1a152ff2e2fc
Romain Dolbeau <romain@dolbeau.org>
2015-11-02 17:29:51 +0000

    popt 1.16

---
+++ popt/build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=popt
+version=1.16
+pkgver=1
+source[0]=ftp://anduin.linuxfromscratch.org/BLFS/${topdir}/${topdir}-${version}.tar.gz
+# If there are no patches, simply comment this
+#patch[0]=
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+#configure_args+=
+
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+export PYTHON=$prefix/bin/python2.7
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS NEWS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
