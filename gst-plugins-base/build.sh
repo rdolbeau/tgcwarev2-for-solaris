3ae234cc65c1e017c67995cadc2952708dcbf851
Romain Dolbeau <romain@dolbeau.org>
2015-11-04 10:36:38 +0000

    gst-plugins-base 1.0.3

---
+++ gst-plugins-base/build.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# This is a buildpkg build.sh script
+# build.sh helper functions
+. ${BUILDPKG_SCRIPTS}/build.sh.functions
+#
+###########################################################
+# Check the following 4 variables before running the script
+topdir=gst-plugins-base
+version=1.0.3
+pkgver=1
+source[0]=http://gstreamer.freedesktop.org/src/${topdir}/${topdir}-${version}.tar.xz
+# If there are no patches, simply comment this
+#BSD ioctls
+patch[0]=gst-plugins-base103-001
+#rfc2553
+patch[1]=gst-plugins-base103-002
+patch[2]=gst-plugins-base103-003
+patch[3]=gst-plugins-base103-004
+#no xkb
+patch[4]=gst-plugins-base103-005
+
+# Source function library
+. ${BUILDPKG_SCRIPTS}/buildpkg.functions
+
+#configure_args+=
+
+export CPPFLAGS="-I$prefix/include"
+export LDFLAGS="-L$prefix/lib -R$prefix/lib"
+export PYTHON=$prefix/bin/python2.7
+
+reg prep
+prep()
+{
+    generic_prep
+}
+
+reg build
+build()
+{
+    generic_build
+}
+
+reg check
+check()
+{
+    generic_check
+}
+
+reg install
+install()
+{
+    generic_install DESTDIR
+    doc COPYING AUTHORS NEWS
+}
+
+reg pack
+pack()
+{
+    generic_pack
+}
+
+reg distclean
+distclean()
+{
+    clean distclean
+}
+
+###################################################
+# No need to look below here
+###################################################
+build_sh $*
